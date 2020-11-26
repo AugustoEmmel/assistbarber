7094356f3edd8aede44f927f5aea30cbec1f7184
Augusto <augusto.emmel@gmail.com>
2020-11-25 21:33:21 -0300

    babel dep

---
+++ server/models/servico.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const servicoSchema = new Schema({
+    nome:{
+        type: String,
+        required: [true, 'Insira o nome do serviço.']
+    },
+    descricao:{
+        type: String,
+        required: [true, 'Insira a descrição do serviço']
+    },
+    preco:{
+        type: Number,
+        required: [true, 'Insira o preço do serviço.']
+    },
+}, {timestamps: true});
+
+const Servico =  mongoose.model('Servico', servicoSchema);
+module.exports = Servico;
