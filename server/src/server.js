7332afd855b2f2032a2f3cdbd399b2794f256799
Augusto <augusto.emmel@gmail.com>
2020-11-28 23:10:41 -0300

    cadastro

--- server/src/server.js
+++ server/src/server.js
@@ -4,6 +4,7 @@ import cors from 'cors'; //cors
 import express from 'express'; //Express
 import usuario from './routes/usuarioRotas'; //Autenticacao
 import './helpers/db/mongodb'; //Banco de dados
+import cookieParser from 'cookie-parser';
 
 // Configurando portas
 const port = process.env.PORT || 5000;
@@ -12,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 // Defininindo rotas para middleware
 app.use('/', usuario);
 // Escutando Portas
